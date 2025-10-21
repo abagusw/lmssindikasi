4f25f5b72a7f11cde89da4a445537f0df183f40e
abagusw <bagusandreas13@gmail.com>
2025-10-21 21:40:56 +0800

    try mailjet

---
+++ app/Controllers/MailjetMailController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controllers;
+
+use \Mailjet\Resources;
+
+class MailjetMailController extends BaseController
+{
+    protected $mailjet;
+
+    public function __construct()
+    {
+        $apiKey = env('mailjet.apiKey', '');
+        $apiSecret = env('mailjet.apiSecret', '');
+
+        $this->mailjet = new \Mailjet\Client($apiKey, $apiSecret, true, ['version' => 'v3.1']);
+    }
+
+    public function sendEmail($toEmail, $toName, $subject, $htmlContent, $textContent)
+    {
+        $body = [
+            'Messages' => [
+                [
+                    'From' => [
+                        'Email' => env('mailjet.senderEmail', 'tech@sindikasi.org'),
+                        'Name' => env('mailjet.senderName', 'Admin Sindikasi')
+                    ],
+                    'To' => [
+                        [
+                            'Email' => $toEmail,
+                            'Name' => $toName
+                        ]
+                    ],
+                    'Subject' => $subject,
+                    'HTMLPart' => $htmlContent,
+                    'TextPart' => $textContent
+                ]
+            ]
+        ];
+
+        $response = $this->mailjet->post(Resources::$Email, ['body' => $body]);
+
+        if ($response->success()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function testSendEmailByMailjet()
+    {
+        $toEmail = 'bagusandreas13@gmail.com';
+        $toName = 'Bagus Andreas';
+        $subject = 'Test Email from Mailjet';
+        $htmlContent = '<h3>This is a test email sent using Mailjet API</h3><p>Hello, this is a sample email body in HTML format.</p>';
+        $textContent = 'This is a test email sent using Mailjet API. Hello, this is a sample email body in plain text format.';
+        $sendMail = $this->sendEmail($toEmail, $toName, $subject, $htmlContent, $textContent);
+        if ($sendMail) {
+            echo 'Email sent successfully via Mailjet!';
+        } else {
+            echo 'Failed to send email via Mailjet.';
+        }
+    }
+}
--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -7,4 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'org\\bovigo\\vfs\\' => array($vendorDir . '/mikey179/vfsstream/src/main/php'),
+    'Mailjet' => array($vendorDir . '/mailjet/mailjet-apiv3-php/src'),
 );
